5f5fcf13fbc39610a6d574f0dd7b1f8285cdf99d
Samprithat-Quinnox <samprithat@quinnox.com>
2020-08-31 21:14:03 +0530

    new build

--- src/main/java/quinnox/sam/projectmanager/webconfig/WebSecurityConfig.java
+++ src/main/java/quinnox/sam/projectmanager/webconfig/WebSecurityConfig.java
@@ -16,7 +16,7 @@ import quinnox.sam.projectmanager.serviceimpl.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-/*@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)*/
+
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public UserDetailsService userDetailsService() {
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/edit/**","/new/**", "/delete/**").hasRole("ADMIN")
+			.antMatchers("/edit/**","/new/**", "/delete/**").hasRole("PM")
 			//.antMatchers("/apply/**").hasRole("USER")
 			.antMatchers("/registration/**" , "/view/**").permitAll()
 			.anyRequest().authenticated()
